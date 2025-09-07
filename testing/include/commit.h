f050bb4cb128adc77da9736949cabbd4be32219e
Anshul Kumar <itsanshulkumar03@gmail.com>
2025-09-07 14:33:51 +0530

    in the process

--- testing/include/commit.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMIT_HPP
-#define COMMIT_HPP
-
-void commit();
-
-#endif // !COMMIT_HPP
--- testing/include/switch.h
+++
@@ -1,9 +0,0 @@
-#ifndef SWITCH_HPP
-#define SWITCH_HPP
-
-#include <string>
-
-void switchBranch(const std::string& branch_name);
-
-
-#endif // !SWITCH_HPP
