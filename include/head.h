783aa186997094d1177890ac48e5d35c61ca4a95
Anshul Kumar <itsanshulkumar03@gmail.com>
2025-12-12 16:07:28 +0530

    bug-fix: switchBranch was not switching the branch when both branched pointed to same commit

--- include/head.h
+++ include/head.h
@@ -7,5 +7,7 @@ std::string getHead();
 void setHead(const std::string& commit_hash);
 std::string getBranchHash(const std::string& branch_name);
 void updateHead(const std::string& branch_name);
+bool isHeadDetached();
+std::string getHeadBranchName();
 
 #endif // !HEAD_HPP
