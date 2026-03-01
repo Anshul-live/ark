8263c8d77dd4e6f95c66162b43377014ed010e0f
Anshul Kumar <itsanshulkumar03@gmail.com>
2026-03-01 23:10:08 +0530

    fix log segmentaion fault

--- src/commands/log.cpp
+++ src/commands/log.cpp
@@ -1,43 +1,46 @@
-#include <iostream>
+#include <ark.h>
+#include <cat-file.h>
+#include <config.h>
 #include <filesystem>
 #include <fstream>
-#include <string>
-#include <sstream>
-#include <cat-file.h>
-#include <ark.h>
+#include <iostream>
 #include <log.h>
 #include <objects.h>
+#include <queue>
 #include <ref.h>
-#include <config.h>
+#include <sstream>
+#include <string>
 
-void logBranch(const std::string& branchName) {
-    Ref ref;
-    Config config;
-    std::string commitHash = ref.getBranchHash(branchName);
-    
-    int depth = -1;
-    while (commitHash != NULL_HASH && (depth == -1 || depth--)) {
-        std::string content = Object::readFromDisk(commitHash);
-        std::vector<std::string> lines = config.split(content, '\n');
-        std::vector<std::string> data;
-        std::cout << "\033[1;33mcommit: " << commitHash << "\033[0m\n";
-        for (auto line : lines) {
-            std::vector<std::string> temp = config.split(line, ' ');
-            data.insert(data.end(), temp.begin(), temp.end());
-        }
-        commitHash = data[3];
-    }
+void logBranch(const std::string &branchName) {
+  Ref ref;
+  Config config;
+  std::string commitHash = ref.getBranchHash(branchName);
+  std::queue<std::string> commit_hashes_queue;
+
+  while (commitHash != NULL_HASH) {
+    std::string content = Object::readFromDisk(commitHash);
+    std::vector<std::string> lines = config.split(content, '\n');
+    std::vector<std::string> data;
+    std::cout << "\033[1;33mcommit: " << commitHash << "\033[0m\n";
+    Commit c;
+    c.loadFromDisk(commitHash);
+    for (auto parent : c.parents)
+      commit_hashes_queue.push(parent);
+    if (commit_hashes_queue.empty())
+      break;
+    commitHash = commit_hashes_queue.front();
+  }
 }
 
-int cmd_log(const std::vector<std::string> &args){
-    Ref ref;
-    std::string currentBranch = ref.getCurrentBranchName();
-    
-    if (currentBranch.empty()) {
-        std::cout << "HEAD is detached\n";
-        return 1;
-    }
-    
-    logBranch(currentBranch);
-    return 0;
+int cmd_log(const std::vector<std::string> &args) {
+  Ref ref;
+  std::string currentBranch = ref.getCurrentBranchName();
+
+  if (currentBranch.empty()) {
+    std::cout << "HEAD is detached\n";
+    return 1;
+  }
+
+  logBranch(currentBranch);
+  return 0;
 }
